5abd057fb2fd776bd2c4fdbdc471f4d2d34d398b
ikurowski <igorkurowski@gmail.com>
2023-02-01 20:29:26 +0100

    add styles.ts

---
+++ src/styles/styles.ts
@@ -0,0 +1,18 @@
+import {moderateScale} from 'react-native-size-matters';
+
+export const theme = {
+  color: {
+    primary: '#fff',
+    secondary: '#fff',
+    tetriary: '#fff',
+    quaternary: '#fff',
+    quinary: '#fff',
+  },
+  backgroundColor: {
+    primary: '#fff',
+  },
+  fontSize: {
+    nine: moderateScale(9),
+    thirtySix: moderateScale(36),
+  },
+};
