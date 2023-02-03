349cc7911af8fb9a2a9eee6c455c48171d41486b
ikurowski <igorkurowski@gmail.com>
2023-02-03 12:08:21 +0100

    add theme

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -2,17 +2,31 @@ import {moderateScale} from 'react-native-size-matters';
 
 export const theme = {
   color: {
-    primary: '#fff',
-    secondary: '#fff',
-    tetriary: '#fff',
-    quaternary: '#fff',
-    quinary: '#fff',
+    primary: '#E63946',
+    secondary: '#F1FAEE',
+    tetriary: '#A8DADC',
+    quaternary: '#457B9D',
+    quinary: '#1D3557',
   },
   backgroundColor: {
-    primary: '#fff',
+    primary: '#1D3557',
   },
   fontSize: {
     nine: moderateScale(9),
     thirtySix: moderateScale(36),
+  },
+};
+
+export const navigationTheme = {
+  primary: {
+    dark: false,
+    colors: {
+      primary: 'E63946',
+      background: '#1D3557',
+      card: '#1D3557',
+      text: '#F1FAEE',
+      border: '#1D3557',
+      notification: '#E63946',
+    },
   },
 };
