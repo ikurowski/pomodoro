b0e59f343fa6b68f4a63f6a9debca2fb3d38cf97
ikurowski <igorkurowski@gmail.com>
2023-02-06 16:05:27 +0100

    Timer comp WIP

---
+++ src/components/Timer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {RenderCounter} from '../utils/utils';
+import PoppinsRegular from './fonts/PoppinsRegular';
+
+function Timer() {
+  return (
+    <View style={styles.container}>
+      <PoppinsRegular>Timer</PoppinsRegular>
+      <RenderCounter message="Timer" />
+    </View>
+  );
+}
+
+export default Timer;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
