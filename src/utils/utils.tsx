7b9b2e8d52b18325b949510d334fabfeaa525426
ikurowski <igorkurowski@gmail.com>
2023-02-07 15:52:13 +0100

    feat(utils): add absolute style

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {useRef} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 export const RenderCounter = ({message}: {message: string}) => {
   const renderCounter = useRef(0);
   renderCounter.current = renderCounter.current + 1;
   return (
-    <View>
+    <View style={styles.container}>
       <Text>
         Renders: {renderCounter.current}, {message}
       </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+});
