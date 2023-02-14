6d6d2f30b4eb4b3f1f28c7553701a9efc1303721
ikurowski <igorkurowski@gmail.com>
2023-02-14 19:12:24 +0100

    feat(millisecondsToTime): add hours

--- src/utils/millisecondsToTime.ts
+++ src/utils/millisecondsToTime.ts
@@ -1,11 +1,23 @@
-const millisecondsToTime = (milliseconds: number) => {
+const millisecondsToTime = (
+  milliseconds: number,
+  onlyMinutes: boolean = false,
+) => {
   const seconds = Math.floor((milliseconds / 1000) % 60);
   const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
+  const hours = Math.floor(milliseconds / 1000 / 60 / 60);
 
-  return [
+  if (onlyMinutes) {
+    return hours > 0 ? `${hours * 60 + minutes}` : minutes.toString();
+  }
+
+  const timeArr = [
     minutes.toString().padStart(2, '0'),
     seconds.toString().padStart(2, '0'),
-  ].join(':');
+  ];
+  if (hours > 0) {
+    timeArr.unshift(hours.toString());
+  }
+  return timeArr.join(':');
 };
 
 export default millisecondsToTime;
