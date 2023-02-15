a27591a255c22be40905d85de9c2e5fffbc34ec9
ikurowski <igorkurowski@gmail.com>
2023-02-15 12:53:26 +0100

    test(eslint):

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'react/no-unstable-nested-components': [
+      'warn',
+      {
+        allowAsProps: true,
+      },
+    ],
+  },
 };
