dabcc431712253d7112e3a9fd6349cbe960f9b72
ikurowski <igorkurowski@gmail.com>
2023-02-22 18:55:32 +0100

    feat: add basic button and remove old reset one

--- src/components/buttons/BasicButton.tsx
+++ src/components/buttons/BasicButton.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import {Pressable, StyleSheet} from 'react-native';
+import useTheme from '../../hooks/useTheme/useTheme';
 import NunitoSemiBold from '../fonts/NunitoSemiBold';
 
-function BasicButton({child}: {child: string}) {
+function BasicButton({
+  onPress,
+  children,
+  filled = false,
+  moreStyles,
+}: {
+  onPress: () => void;
+  children: string;
+  filled?: boolean;
+  moreStyles?: any;
+}) {
+  const {
+    navigation: {colors},
+  } = useTheme();
+
   return (
-    <Pressable style={styles.container}>
-      <NunitoSemiBold>{child}</NunitoSemiBold>
+    <Pressable
+      onPress={onPress}
+      style={
+        filled
+          ? {
+              ...styles.filledButton,
+              backgroundColor: colors.primary,
+              ...moreStyles,
+            }
+          : {...styles.nonFilledButton, borderColor: colors.text, ...moreStyles}
+      }>
+      <NunitoSemiBold style={styles.text} size={20}>
+        {children}
+      </NunitoSemiBold>
     </Pressable>
   );
 }
@@ -13,7 +40,24 @@ function BasicButton({child}: {child: string}) {
 export default BasicButton;
 
 const styles = StyleSheet.create({
-  container: {
-    marginVertical: 20,
+  nonFilledButton: {
+    borderWidth: 1,
+    borderRadius: 60,
+    paddingHorizontal: 30,
+    paddingVertical: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  filledButton: {
+    borderRadius: 60,
+    paddingHorizontal: 30,
+    paddingVertical: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  text: {
+    textAlign: 'center',
   },
 });
