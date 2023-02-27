6cb3944149483fe7cb920d002a89af7db2f62d31
ikurowski <igorkurowski@gmail.com>
2023-02-27 18:14:54 +0100

    feat: change BG color for breaks

--- src/hooks/useTheme/useTheme.tsx
+++ src/hooks/useTheme/useTheme.tsx
@@ -1,9 +1,20 @@
 import {useContext} from 'react';
+import {useSelector} from 'react-redux';
+import {colors} from '../../styles/styles';
+import {RootState} from '../../types/types';
 
 import ThemeContext from './ThemeContext';
 
 export default function useTheme() {
   const theme = useContext(ThemeContext);
+  const {currentTimerType} = useSelector((state: RootState) => state.timer);
+
+  if (currentTimerType !== 'pomodoroTimeInMS') {
+    theme.navigation.colors.background = colors.tertiary;
+  }
+  if (currentTimerType === 'pomodoroTimeInMS') {
+    theme.navigation.colors.background = colors.secondary;
+  }
 
   return theme;
 }
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,17 +1,27 @@
 import {moderateScale} from 'react-native-size-matters';
 import {Theme} from '../types/types';
 
+export const colors = {
+  primary: '#E39C0C',
+  secondary: '#40916C',
+  tertiary: '#48A750',
+  white: '#FFFFFF',
+  black: '#000000',
+  grey: '#E5E5E5',
+  lightGrey: '#F5F5F5',
+};
+
 export const theme: Theme = {
   // I used react-navigation theme structure to match default react-navigation theme
   navigation: {
     dark: false,
     colors: {
-      primary: '#E39C0C',
-      background: '#40916C',
-      border: '#000000',
-      card: '#40916C',
-      text: '#FFFFFF',
-      notification: '#E39C0C',
+      primary: colors.primary,
+      background: colors.secondary,
+      border: colors.black,
+      card: colors.secondary,
+      text: colors.white,
+      notification: colors.primary,
     },
   },
   fontSize: {
