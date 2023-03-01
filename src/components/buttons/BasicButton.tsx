fbcdc6606a889082c640ffc3cbe0d3f5cbfd9a44
ikurowski <igorkurowski@gmail.com>
2023-03-01 16:21:35 +0100

    refactor(BasicButton): DRY

--- src/components/buttons/BasicButton.tsx
+++ src/components/buttons/BasicButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, StyleSheet} from 'react-native';
+import {Pressable, StyleSheet, ViewStyle} from 'react-native';
 import useTheme from '../../hooks/useTheme/useTheme';
 import NunitoSemiBold from '../fonts/NunitoSemiBold';
 
@@ -39,23 +39,22 @@ function BasicButton({
 
 export default BasicButton;
 
+const sharedStyles: ViewStyle = {
+  borderRadius: 60,
+  paddingHorizontal: 30,
+  paddingVertical: 12,
+  alignItems: 'center',
+  justifyContent: 'center',
+  flex: 1,
+};
+
 const styles = StyleSheet.create({
   nonFilledButton: {
     borderWidth: 1,
-    borderRadius: 60,
-    paddingHorizontal: 30,
-    paddingVertical: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
+    ...sharedStyles,
   },
   filledButton: {
-    borderRadius: 60,
-    paddingHorizontal: 30,
-    paddingVertical: 12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
+    ...sharedStyles,
   },
   text: {
     textAlign: 'center',
