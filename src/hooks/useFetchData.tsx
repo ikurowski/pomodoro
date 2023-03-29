c481dddc7089b1c19365d755a183021c910c90ec
ikurowski <igorkurowski@gmail.com>
2023-03-29 16:48:40 +0200

    refactor: extract fetching logic to separate hook

---
+++ src/hooks/useFetchData.tsx
@@ -0,0 +1,83 @@
+import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {updateTasks} from '../features/tasksSlice';
+import {
+  updateRepeats,
+  updateSchedule,
+  updateSettings,
+  updateTime,
+} from '../features/timerSettingsSlice';
+import {getAsyncData} from '../stores/RNAsyncStorage';
+import {IntervalType, STORAGE_KEY} from '../types/types';
+
+const useFetchAsyncData = () => {
+  //TODO this fetching can be optimized by using multiGetAsyncData
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const [
+          pomodoroTimeInMS,
+          shortBreakTimeInMS,
+          longBreakTimeInMS,
+          sound,
+          vibration,
+          repeats,
+          breaks,
+          tasks,
+          schedule,
+        ] = await Promise.all([
+          getAsyncData(STORAGE_KEY.FOCUS_TIME),
+          getAsyncData(STORAGE_KEY.SHORT_BREAK_TIME),
+          getAsyncData(STORAGE_KEY.LONG_BREAK_TIME),
+          getAsyncData(STORAGE_KEY.SOUND),
+          getAsyncData(STORAGE_KEY.VIBRATION),
+          getAsyncData(STORAGE_KEY.REPEATS),
+          getAsyncData(STORAGE_KEY.BREAKS),
+          getAsyncData(STORAGE_KEY.TASKS),
+          getAsyncData(STORAGE_KEY.SCHEDULE),
+        ]);
+
+        const updateTimeDispatch = (
+          type: IntervalType,
+          wheelPickerTime: number,
+        ) => {
+          dispatch(updateTime({type, wheelPickerTime}));
+        };
+
+        if (pomodoroTimeInMS !== null) {
+          updateTimeDispatch('pomodoroTimeInMS', pomodoroTimeInMS);
+        }
+        if (shortBreakTimeInMS !== null) {
+          updateTimeDispatch('shortBreakTimeInMS', shortBreakTimeInMS);
+        }
+        if (longBreakTimeInMS !== null) {
+          updateTimeDispatch('longBreakTimeInMS', longBreakTimeInMS);
+        }
+        if (repeats !== null) {
+          dispatch(updateRepeats({repeats: repeats}));
+        }
+        if (sound !== null) {
+          dispatch(updateSettings({property: 'sound', value: sound}));
+        }
+        if (vibration !== null) {
+          dispatch(updateSettings({property: 'vibration', value: vibration}));
+        }
+        if (breaks !== null) {
+          dispatch(updateSettings({property: 'breaks', value: breaks}));
+        }
+        if (tasks !== null) {
+          dispatch(updateTasks(tasks));
+        }
+        if (schedule !== null) {
+          dispatch(updateSchedule({schedule: schedule}));
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, [dispatch]);
+};
+
+export default useFetchAsyncData;
--- src/navigation/Index.tsx
+++ src/navigation/Index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
@@ -13,17 +13,10 @@ import useTheme from '../hooks/useTheme/useTheme';
 import TabBar from './TabBar';
 
 //storages
-import {useDispatch, useSelector} from 'react-redux';
-import {
-  updateSettings,
-  updateTime,
-  updateRepeats,
-} from '../features/timerSettingsSlice';
-import {getAsyncData} from '../stores/RNAsyncStorage';
+import {useSelector} from 'react-redux';
 
 //types
-import {IntervalType, TimerRootState, STORAGE_KEY} from '../types/types';
-import {updateTasks} from '../features/tasksSlice';
+import {TimerRootState} from '../types/types';
 
 const Tab = createMaterialTopTabNavigator<RootStackParamList>();
 
@@ -38,68 +31,6 @@ function Navigation() {
   const {isRunning, isPaused} = useSelector(
     (reduxState: TimerRootState) => reduxState.timer,
   );
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const [
-          pomodoroTimeInMS,
-          shortBreakTimeInMS,
-          longBreakTimeInMS,
-          sound,
-          vibration,
-          repeats,
-          breaks,
-          tasks,
-        ] = await Promise.all([
-          getAsyncData(STORAGE_KEY.FOCUS_TIME),
-          getAsyncData(STORAGE_KEY.SHORT_BREAK_TIME),
-          getAsyncData(STORAGE_KEY.LONG_BREAK_TIME),
-          getAsyncData(STORAGE_KEY.SOUND),
-          getAsyncData(STORAGE_KEY.VIBRATION),
-          getAsyncData(STORAGE_KEY.REPEATS),
-          getAsyncData(STORAGE_KEY.BREAKS),
-          getAsyncData(STORAGE_KEY.TASKS),
-        ]);
-
-        const updateTimeDispatch = (
-          type: IntervalType,
-          wheelPickerTime: number,
-        ) => {
-          dispatch(updateTime({type, wheelPickerTime}));
-        };
-
-        if (pomodoroTimeInMS !== null) {
-          updateTimeDispatch('pomodoroTimeInMS', pomodoroTimeInMS);
-        }
-        if (shortBreakTimeInMS !== null) {
-          updateTimeDispatch('shortBreakTimeInMS', shortBreakTimeInMS);
-        }
-        if (longBreakTimeInMS !== null) {
-          updateTimeDispatch('longBreakTimeInMS', longBreakTimeInMS);
-        }
-        if (repeats !== null) {
-          dispatch(updateRepeats({repeats: repeats}));
-        }
-        if (sound !== null) {
-          dispatch(updateSettings({property: 'sound', value: sound}));
-        }
-        if (vibration !== null) {
-          dispatch(updateSettings({property: 'vibration', value: vibration}));
-        }
-        if (breaks !== null) {
-          dispatch(updateSettings({property: 'breaks', value: breaks}));
-        }
-        if (tasks !== null) {
-          dispatch(updateTasks(tasks));
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    })();
-  }, [dispatch]);
 
   return (
     <NavigationContainer theme={navigationTheme}>
